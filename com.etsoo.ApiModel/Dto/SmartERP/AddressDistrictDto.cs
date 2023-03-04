4b7cbfcfcadf7f5d9ea0aa5fa559645941ef8288
Garry Xiao <xz@etsoo.com>
2023-03-04 16:32:14 +1300

    Add AddressCityDto and AddressDistrictDto

---
+++ com.etsoo.ApiModel/Dto/SmartERP/AddressDistrictDto.cs
@@ -0,0 +1,27 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP
+{
+    /// <summary>
+    /// Address district data
+    /// 区县地址数据
+    /// </summary>
+    public record AddressDistrictDto
+    {
+        /// <summary>
+        /// Id
+        /// 编号
+        /// </summary>
+        public required int Id { get; init; }
+
+        /// <summary>
+        /// Num Id
+        /// 数字编号
+        /// </summary>
+        public string? Num { get; init; }
+
+        /// <summary>
+        /// Label
+        /// 标签
+        /// </summary>
+        public required string Label { get; init; }
+    }
+}
