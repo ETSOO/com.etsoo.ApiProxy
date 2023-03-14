9c099e91404677079ab0898f2b2e7b8fb59cb3f7
Garry Xiao <xz@etsoo.com>
2023-03-14 23:43:34 +1300

    Add ParsePinRQ and PinDto

---
+++ com.etsoo.ApiModel/Dto/SmartERP/GetAddressNumDto.cs
@@ -0,0 +1,57 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP
+{
+    /// <summary>
+    /// Get address data by Num
+    /// 从数字编号获取地址信息
+    /// </summary>
+    public record GetAddressNumDto
+    {
+        /// <summary>
+        /// Region or country id, like CN for China
+        /// 地区或国家编号，比如 CN 表示中国
+        /// </summary>
+        public string? RegionId { get; init; }
+
+        /// <summary>
+        /// State
+        /// 州省
+        /// </summary>
+        public string? State { get; init; }
+
+        /// <summary>
+        /// State id
+        /// 州省编号
+        /// </summary>
+        public string? StateId { get; init; }
+
+        /// <summary>
+        /// City
+        /// 城市
+        /// </summary>
+        public string? City { get; init; }
+
+        /// <summary>
+        /// City id
+        /// 城市编号
+        /// </summary>
+        public int? CityId { get; init; }
+
+        /// <summary>
+        /// District
+        /// 区县
+        /// </summary>
+        public string? District { get; init; }
+
+        /// <summary>
+        /// District id
+        /// 区县编号
+        /// </summary>
+        public int? DistrictId { get; init; }
+
+        /// <summary>
+        /// Merged district
+        /// 合并的区县名称
+        /// </summary>
+        public string? MergedDistrict { get; init; }
+    }
+}
---
+++ com.etsoo.ApiModel/Dto/SmartERP/PinDto.cs
@@ -0,0 +1,27 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP
+{
+    /// <summary>
+    /// PIN data
+    /// 身份证信息
+    /// </summary>
+    public record PinDto : GetAddressNumDto
+    {
+        /// <summary>
+        /// Birthday
+        /// 出生日期
+        /// </summary>
+        public DateTime? Birthday { get; init; }
+
+        /// <summary>
+        /// Gender
+        /// 性别
+        /// </summary>
+        public string? Gender { get; init; }
+
+        /// <summary>
+        /// Whether the form is legal
+        /// 是否形式合法
+        /// </summary>
+        public required bool Valid { get; init; }
+    }
+}
