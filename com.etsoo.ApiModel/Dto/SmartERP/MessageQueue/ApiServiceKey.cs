4012bb2f4aad8905779fd51f9236afeea78ade1c
Garry Xiao <xz@etsoo.com>
2023-06-25 21:42:55 +1200

    Add MessageQueue data models

---
+++ com.etsoo.ApiModel/Dto/SmartERP/MessageQueue/ApiServiceKey.cs
@@ -0,0 +1,27 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP.MessageQueue
+{
+    /// <summary>
+    /// API service key data
+    /// 接口服务键数据
+    /// </summary>
+    public record ApiServiceKey
+    {
+        /// <summary>
+        /// Organization
+        /// 所属机构
+        /// </summary>
+        public required string Organization { get; init; }
+
+        /// <summary>
+        /// Current user
+        /// 当前用户
+        /// </summary>
+        public required string User { get; init; }
+
+        /// <summary>
+        /// Time ticks
+        /// 时间刻度
+        /// </summary>
+        public long Ticks { get; init; } = DateTime.UtcNow.Ticks;
+    }
+}
---
+++ com.etsoo.ApiModel/Dto/SmartERP/MessageQueue/SendEmailDto.cs
@@ -0,0 +1,39 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP.MessageQueue
+{
+    /// <summary>
+    /// Send email data
+    /// 发送邮件信息
+    /// </summary>
+    public record SendEmailDto
+    {
+        /// <summary>
+        /// Message Type
+        /// 消息类型
+        /// </summary>
+        public const string MessageType = "SMARTERP-SENDEMAIL";
+
+        /// <summary>
+        /// Subject
+        /// 主题
+        /// </summary>
+        public required string Subject { get; init; }
+
+        /// <summary>
+        /// Body
+        /// 主体
+        /// </summary>
+        public required string Body { get; init; }
+
+        /// <summary>
+        /// Recipients
+        /// 收件人
+        /// </summary>
+        public required IEnumerable<int> Users { get; init; }
+
+        /// <summary>
+        /// Delivery time
+        /// 发送时间
+        /// </summary>
+        public DateTime? DeliveryTime { get; init; }
+    }
+}
