d88214bb29590e475a43e38c7eb83cdf7a6f835c
Garry Xiao <xz@etsoo.com>
2023-08-09 22:48:29 +1200

    Add OperationMessageDto

---
+++ com.etsoo.ApiModel/Dto/SmartERP/MessageQueue/OperationMessageDto.cs
@@ -0,0 +1,33 @@
+﻿namespace com.etsoo.ApiModel.Dto.SmartERP.MessageQueue
+{
+    /// <summary>
+    /// Operation message data
+    /// 操作信息数据
+    /// </summary>
+    public record OperationMessageDto
+    {
+        /// <summary>
+        /// Organization id
+        /// 机构编号
+        /// </summary>
+        public required int OrganizationId { get; init; }
+
+        /// <summary>
+        /// User id
+        /// 用户编号
+        /// </summary>
+        public required int UserId { get; init; }
+
+        /// <summary>
+        /// Target object id
+        /// 目标对象编号
+        /// </summary>
+        public long? Id { get; init; }
+
+        /// <summary>
+        /// Operation type
+        /// 操作类型
+        /// </summary>
+        public required string OperationType { get; init; }
+    }
+}
