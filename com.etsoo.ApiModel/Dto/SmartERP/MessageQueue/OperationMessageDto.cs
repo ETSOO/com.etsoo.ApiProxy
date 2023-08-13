f4c186118f54741f4fb0e5c15d135b7cb7e1216b
Garry Xiao <xz@etsoo.com>
2023-08-13 15:31:32 +1200

    Add DeviceId to OperationMessageResponse

--- com.etsoo.ApiModel/Dto/SmartERP/MessageQueue/OperationMessageDto.cs
+++ com.etsoo.ApiModel/Dto/SmartERP/MessageQueue/OperationMessageDto.cs
@@ -13,10 +13,16 @@
         public required string AppId { get; init; }
 
         /// <summary>
-        /// User id
-        /// 用户编号
+        /// Global user id
+        /// 全局用户编号
         /// </summary>
         public required int UserId { get; init; }
+
+        /// <summary>
+        /// Current device id, set 0 when not existing
+        /// 当前设备编号，不存在可设置为0
+        /// </summary>
+        public required int DeviceId { get; init; }
 
         /// <summary>
         /// Target object id
