b65d83269b47a94a4c24702473314d19557d365d
Garry Xiao <xz@etsoo.com>
2024-07-10 22:35:15 +1200

    Add GetSignInUrl and GetSignUpUrl

---
+++ com.etsoo.ApiModel/Auth/AuthExtentions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace com.etsoo.ApiModel.Auth
+{
+    /// <summary>
+    /// Auth extentions
+    /// 授权扩展
+    /// </summary>
+    public static class AuthExtentions
+    {
+        /// <summary>
+        /// Sign in action
+        /// 登录动作
+        /// </summary>
+        public const string SignInAction = "SignIn";
+
+        /// <summary>
+        /// Sign up action
+        /// 注册动作
+        /// </summary>
+        public const string SignUpAction = "SignUp";
+
+        /// <summary>
+        /// Get request action
+        /// 获取请求动作
+        /// </summary>
+        /// <param name="request">HTTP Request</param>
+        /// <returns>Action</returns>
+        public static string GetRequestAction(HttpRequest request)
+        {
+            var action = request.RouteValues.LastOrDefault().Value?.ToString()
+                ?? (Uri.TryCreate(request.Path, UriKind.Absolute, out var uri) ? uri.Segments.Last() : string.Empty);
+            return action.Trim('/');
+        }
+    }
+}
--- com.etsoo.ApiModel/Auth/IAuthClient.cs
+++ com.etsoo.ApiModel/Auth/IAuthClient.cs
@@ -10,25 +10,44 @@ namespace com.etsoo.ApiModel.Auth
     public interface IAuthClient
     {
         /// <summary>
+        /// Get sign in URL
+        /// 获取登录URL
+        /// </summary>
+        /// <param name="state">Request state</param>
+        /// <param name="loginHint">Login hint</param>
+        /// <returns>URL</returns>
+        string GetSignInUrl(string state, string? loginHint = null);
+
+        /// <summary>
+        /// Get sign up URL
+        /// 获取注册URL
+        /// </summary>
+        /// <param name="state">Request state</param>
+        /// <returns>URL</returns>
+        string GetSignUpUrl(string state);
+
+        /// <summary>
         /// Get server auth URL, for back-end processing
         /// 获取服务器授权URL，用于后端处理
         /// </summary>
+        /// <param name="action">Action of the request</param>
         /// <param name="state">Request state</param>
         /// <param name="scope">Scope</param>
         /// <param name="offline">Is offline mode to generate refresh token</param>
         /// <param name="loginHint">Login hint</param>
         /// <returns>URL</returns>
-        string GetServerAuthUrl(string state, string scope, bool offline = false, string? loginHint = null);
+        string GetServerAuthUrl(string action, string state, string scope, bool offline = false, string? loginHint = null);
 
         /// <summary>
         /// Get script auth URL, for front-end page
         /// 获取脚本授权URL，用于前端页面
         /// </summary>
+        /// <param name="action">Action of the request</param>
         /// <param name="state">Request state</param>
         /// <param name="scope">Scope</param>
         /// <param name="loginHint">Login hint</param>
         /// <returns>URL</returns>
-        string GetScriptAuthUrl(string state, string scope, string? loginHint = null);
+        string GetScriptAuthUrl(string action, string state, string scope, string? loginHint = null);
 
         /// <summary>
         /// Get user info from callback request
@@ -36,8 +55,9 @@ namespace com.etsoo.ApiModel.Auth
         /// </summary>
         /// <param name="request">Callback request</param>
         /// <param name="state">Request state</param>
+        /// <param name="action">Request action</param>
         /// <returns>Action result & user information</returns>
-        ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, string state);
+        ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, string state, string? action = null);
 
         /// <summary>
         /// Get user info from callback request
@@ -45,7 +65,8 @@ namespace com.etsoo.ApiModel.Auth
         /// </summary>
         /// <param name="request">Callback request</param>
         /// <param name="stateCallback">Callback to verify request state</param>
+        /// <param name="action">Request action</param>
         /// <returns>Action result & user information</returns>
-        ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, Func<string, bool> stateCallback);
+        ValueTask<(IActionResult result, AuthUserInfo? userInfo)> GetUserInfoAsync(HttpRequest request, Func<string, bool> stateCallback, string? action = null);
     }
 }
