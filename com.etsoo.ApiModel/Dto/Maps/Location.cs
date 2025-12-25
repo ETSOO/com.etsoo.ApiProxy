46730fa4aaead35e7fee0075bbc8c0c4b71d3e64
Garry Xiao <xz@etsoo.com>
2025-12-25 08:52:19 +0800

    Add Location.ToLngLatString

--- com.etsoo.ApiModel/Dto/Maps/Location.cs
+++ com.etsoo.ApiModel/Dto/Maps/Location.cs
@@ -8,11 +8,31 @@
     /// <param name="Lng">Longitude, 经度</param>
     public record Location(float Lat, float Lng)
     {
+        /// <summary>
+        /// To LatLng string
+        /// 转换为 LatLng 字符串
+        /// </summary>
+        /// <returns>Result</returns>
         public override string ToString()
         {
             return $"{Lat},{Lng}";
         }
 
+        /// <summary>
+        /// To LngLat string
+        /// 转换为 LngLat 字符串
+        /// </summary>
+        /// <returns>Result</returns>
+        public string ToLngLatString()
+        {
+            return $"{Lng},{Lat}";
+        }
+
+        /// <summary>
+        /// To SQL Geography format
+        /// 转换为 SQL Geography 格式
+        /// </summary>
+        /// <returns>Result</returns>
         public string ToSqlGeography()
         {
             return $"POINT({Lat} {Lng})";
